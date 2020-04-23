2371f7229bbba8ba9fdb3cdd3ef4c3d71031ed76
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-04-23 18:00:35 +0700

    feat: add auto writefile

---
+++ toJson.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+const main = async () => {
+	try {
+		const readmeContent = await fs.readFileSync(path.resolve(`./README.md`), { encoding: 'utf-8' });
+		const regex = /^\#{3}.*\n\n- Waktu.*\n- Pemateri.*\n- Slide.*\n- Video.*\n.*\n.*/gm;
+		const matchContent = readmeContent.match(regex);
+		const allData = []
+
+		matchContent.forEach(ctx => {
+			if (!ctx.startsWith('### Template sesi selanjutnya')) {
+				const dateRegex = /^- Waktu.*/gm;
+				const pemateriRegex = /^- Pemateri.*/gm;
+				const slideRegex = /^- Slide.*/gm;
+				const TopicRegex = /^\#{3}.*/gm;
+				const VideoRegex = /- Video.*\n.*\n.*/gm;
+				const videosRaw = ctx.match(VideoRegex)[0].replace('- Video: ', '').trim();
+				const splitVideos = videosRaw.split('\n').map(i => i.replace('- ', '').trim())
+				allData.push({
+					"date": ctx.match(dateRegex)[0].replace('- Waktu: ', '').trim(),
+					"speaker": ctx.match(pemateriRegex)[0].replace('- Pemateri: ', '').trim(),
+					"slide": ctx.match(slideRegex)[0].replace('- Slide:', '').trim(),
+					"topic": ctx.match(TopicRegex)[0].replace('### ', '').trim(),
+					"videos": splitVideos,
+				})
+			}
+		});
+
+		fs.writeFile('./data.json', JSON.stringify({
+			data: allData
+		}), function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(data);
+		});
+
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+main();
