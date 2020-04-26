45d19ffb0387d4fb55e48d2f9f541e0f397390b2
Luri <luri@kioss.com>
2020-04-27 05:10:00 +0700

    Add event time and url

--- toJson.js
+++ toJson.js
@@ -4,14 +4,16 @@ const path = require('path');
 const main = async () => {
 	try {
 		const readmeContent = await fs.readFileSync(path.resolve(`./README.md`), { encoding: 'utf-8' });
-		const regex = /^\#{3}.*\n\n- Waktu.*\n- Pemateri.*\n- Slide.*\n- Video.*\n.*\n.*/gm;
+		const regex = /^\#{3}.*\n\n- Waktu.*\n- Pukul.*\n- Pemateri.*\n- URL.*\n- Slide.*\n- Video.*\n.*\n.*/gm;
 		const matchContent = readmeContent.match(regex);
 		const allData = []
 
 		matchContent.forEach(ctx => {
 			if (!ctx.startsWith('### Template')) {
 				const dateRegex = /^- Waktu.*/gm;
+				const timeRegex = /^- Pukul.*/gm;
 				const pemateriRegex = /^- Pemateri.*/gm;
+				const urlRegex = /^- URL.*/gm;
 				const slideRegex = /^- Slide.*/gm;
 				const TopicRegex = /^\#{3}.*/gm;
 				const VideoRegex = /- Video.*\n.*\n.*/gm;
@@ -19,7 +21,9 @@ const main = async () => {
 				const splitVideos = videosRaw.split('\n').map(i => i.replace('- ', '').replace('Registrasi: ', '').trim())
 				allData.push({
 					"date": ctx.match(dateRegex)[0].replace('- Waktu: ', '').trim(),
+					"time": ctx.match(timeRegex)[0].replace('- Pukul: ', '').trim(),
 					"speaker": ctx.match(pemateriRegex)[0].replace('- Pemateri: ', '').trim(),
+					"url": ctx.match(urlRegex)[0].replace('- URL:', '').trim(),
 					"slide": ctx.match(slideRegex)[0].replace('- Slide:', '').trim(),
 					"topic": ctx.match(TopicRegex)[0].replace('### ', '').trim(),
 					"videos": splitVideos,
