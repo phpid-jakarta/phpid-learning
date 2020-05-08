bbe549b85a3ab1215b4c3dc57ddbea15aea644d6
Irfan Maulana <mazipanneh@gmail.com>
2020-05-08 21:55:27 +0700

    Update toJson.js

--- toJson.js
+++ toJson.js
@@ -6,7 +6,7 @@ const getCoverUrl = (idx) => `https://github.com/phpid-jakarta/phpid-online-lear
 const main = async () => {
 	try {
 		const readmeContent = await fs.readFileSync(path.resolve(`./README.md`), { encoding: 'utf-8' });
-		const regex = /^\#{3}.*\n\n- Waktu.*\n- Pukul.*\n- Pemateri.*\n- URL.*\n- Slide.*\n- Video.*\n.*\n.*/gm;
+		const regex = /^\#{3}.*\n\n- Waktu.*\n- Pukul.*\n- Pemateri.*\n- Slide.*\n- Video.*\n.*\n.*/gm;
 		const matchContent = readmeContent.match(regex);
 		const allData = []
 
@@ -16,27 +16,31 @@ const main = async () => {
 				const dateRegex = /^- Waktu.*/gm;
 				const timeRegex = /^- Pukul.*/gm;
 				const pemateriRegex = /^- Pemateri.*/gm;
-				const urlRegex = /^- URL.*/gm;
 				const slideRegex = /^- Slide.*/gm;
-				const TopicRegex = /^\#{3}.*/gm;
-				const VideoRegex = /- Video.*\n.*\n.*/gm;
-				const videosRaw = ctx.match(VideoRegex)[0].replace('- Video: ', '').trim();
-				const splitVideos = videosRaw.split('\n').map(i => i.replace('- ', '').replace('Registrasi: ', '').trim())
+				const topicRegex = /^\#{3}.*/gm;
+				const videoRegex = /- Video.*\n.*\n.*/gm;
+				const videosRaw = ctx.match(videoRegex)[0].replace('- Video: ', '').trim();
+				const splitVideos = videosRaw.split('\n').map(i => i.replace('- ', '').replace('Registrasi: ', '').trim());
+                                const registerRegex = /- Registrasi.*\n.*\n.*/gm;
 				allData.push({
 					"date": ctx.match(dateRegex)[0].replace('- Waktu: ', '').trim(),
 					"time": ctx.match(timeRegex)[0].replace('- Pukul: ', '').trim(),
 					"speaker": ctx.match(pemateriRegex)[0].replace('- Pemateri: ', '').trim(),
-					"url": ctx.match(urlRegex)[0].replace('- URL:', '').trim(),
 					"slide": ctx.match(slideRegex)[0].replace('- Slide:', '').trim(),
-					"topic": ctx.match(TopicRegex)[0].replace('### ', '').trim(),
+					"topic": ctx.match(topicRegex)[0].replace('### ', '').trim(),
 					"videos": splitVideos,
+                                        // field URL is deprecated, use registrasi field
+                                        "url": ctx.match(registerRegex)[0].replace('- Registrasi: ', '').trim(),
+                                        "registrasi": ctx.match(registerRegex)[0].replace('- Registrasi: ', '').trim(),
                                         "cover": getCoverUrl(sessionIndex),
 				})
 			}
 		});
 
 		fs.writeFile(path.resolve('./data.json'), JSON.stringify({
-			data: allData
+                        message: 'field URL is deprecated, use registrasi',
+                        credits: 'PHPID Community',
+			data: allData,
 		}), function (err) {
 			if (err) {
 				return console.log('❌ Error write file data.json', err);
