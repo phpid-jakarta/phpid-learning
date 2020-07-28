b07a8647d6acc23df05f479f712aebccd857a56b
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-07-28 16:23:27 +0700

    fix: heading

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,10 @@
 <script>
   import { allData } from "../store";
+
+  const dataLength = $allData.length;
+  const dataStart = $allData[0].date.split(',')[1].trim();
+  const dataEnd = $allData[dataLength - 1].date.split(',')[1].trim();
+  const dateRange = `${dataStart} - ${dataEnd}`;
 </script>
 
 <style>
@@ -91,11 +96,20 @@
   <div class="hero-image">
     <div class="hero-text">
       <h1 class="animate__animated animate__bounce">PHPID Online Learning 2020</h1>
-      <h3 class="animate__animated animate__bounce">{$allData.length} Materi</h3><span class="animate__animated animate__bounce">{$allData[0].date} - {$allData[$allData.length - 1].date}</span><a href="https://github.com/phpid-jakarta/phpid-online-learning-2020/" target="_blank" rel="noopener" title="Github">
-      <button class="animate__animated animate__bounceIn">Github</button></a><a href="https://www.facebook.com/groups/phpid/" target="_blank" rel="noopener" title="Facebook Group PHPID">
-      <button class="btn-secondary animate__animated animate__bounceIn">FB Group</button></a>
+      <h3 class="animate__animated animate__bounce">{$allData.length} Materi</h3>
+      <span class="animate__animated animate__bounce">{dateRange}</span>
+      <a href="https://github.com/phpid-jakarta/phpid-online-learning-2020/" target="_blank" rel="noopener" title="Github">
+        <button class="animate__animated animate__bounceIn">Github</button>
+      </a>
+      <a href="https://www.facebook.com/groups/phpid/" target="_blank" rel="noopener" title="Facebook Group PHPID">
+        <button class="btn-secondary animate__animated animate__bounceIn">FB Group</button>
+      </a>
     </div>
-    <div class="hero-arrow"><span class="animate__animated animate__bounceIn animate__delay-1s">Lihat Semua Pemateri</span><a href="#content-speaker">
-        <button class="animate__animated animate__bounceIn animate__delay-1s">&darr;</button></a></div>
+    <div class="hero-arrow">
+      <span class="animate__animated animate__bounceIn animate__delay-1s">Lihat Semua Pemateri</span>
+      <a href="#content-speaker">
+        <button class="animate__animated animate__bounceIn animate__delay-1s">&darr;</button>
+      </a>
+    </div>
   </div>
 </header>
