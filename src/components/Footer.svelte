5a478c4fb4c6ee4358ee9b28ea702cf7bf15374d
Irfan Maulana <mazipanneh@gmail.com>
2020-08-12 17:09:29 +0700

    feat: add contributors link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -14,7 +14,15 @@
   </section>
   <section class="row flex-center">
     <div class="text-center">
-      <span>Crafted with ❤️ by PHPID Dev Teams, © 2020</span>
+      <span>Crafted with ❤️ by PHPID Dev Teams and 
+        <a 
+           href="https://github.com/phpid-jakarta/phpid-online-learning-2020/graphs/contributors"
+           target="_blank"
+           rel="noopener"
+           title="Github"
+        >
+          many awesome contributors
+        </a>, © 2020</span>
     </div>
   </section>
 </footer>
