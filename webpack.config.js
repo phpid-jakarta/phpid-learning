47cbed33a0b7c12b62f531345316f130b3f21d63
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-04-09 22:01:39 +0700

    feat: change the way get date

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const webpack = require('webpack')
-const path = require('path')
-const WebpackModules = require('webpack-modules');
-const config = require('sapper/config/webpack.js')
-const sveltePreprocess = require('svelte-preprocess')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const pkg = require('./package.json')
+const webpack = require("webpack");
+const path = require("path");
+const WebpackModules = require("webpack-modules");
+const config = require("sapper/config/webpack.js");
+const sveltePreprocess = require("svelte-preprocess");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const pkg = require("./package.json");
 
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') }
-const extensions = ['.mjs', '.js', '.json', '.svelte', '.html']
-const mainFields = ['svelte', 'module', 'browser', 'main']
+const alias = { svelte: path.resolve("node_modules", "svelte") };
+const extensions = [".mjs", ".js", ".json", ".svelte", ".html"];
+const mainFields = ["svelte", "module", "browser", "main"];
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.svelte', './src/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 const preprocess = sveltePreprocess({
   scss: {
-    includePaths: ['src']
+    includePaths: ["src"],
   },
   postcss: {
     plugins: [
-      require('autoprefixer')(),
-      require('postcss-nested')(),
-      dev ? null : purgecss
-    ].filter(Boolean)
-  }
-})
+      require("autoprefixer")(),
+      require("postcss-nested")(),
+      dev ? null : purgecss,
+    ].filter(Boolean),
+  },
+});
 
 const fileLoader = {
   test: /\.(webp|png|jpe?g|gif)$/i,
-  loader: 'file-loader',
+  loader: "file-loader",
   options: {
-    name: dev ? '[name].[ext]' : '[name].[contenthash].[ext]',
-    publicPath: '/client'
-  }
-}
+    name: dev ? "[name].[ext]" : "[name].[contenthash].[ext]",
+    publicPath: "/client",
+  },
+};
 
 const cssLoader = {
   test: /\.css$/,
@@ -49,9 +49,9 @@ const cssLoader = {
       //   publicPath: 'phpid-learning'
       // }
     },
-    'css-loader'
-  ]
-}
+    "css-loader",
+  ],
+};
 
 module.exports = {
   client: {
@@ -63,71 +63,71 @@ module.exports = {
         {
           test: /\.(svelte|html)$/,
           use: {
-            loader: 'svelte-loader',
+            loader: "svelte-loader",
             options: {
               dev,
               emitCss: true,
               hydratable: true,
               hotReload: false, // pending https://github.com/sveltejs/svelte/issues/2377
-              preprocess
-            }
-          }
+              preprocess,
+            },
+          },
         },
         fileLoader,
-        cssLoader
-      ]
+        cssLoader,
+      ],
     },
     mode,
     plugins: [
       // pending https://github.com/sveltejs/svelte/issues/2377
       // dev && new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode)
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+        "process.env.SAPPER_TIMESTAMP":
+          process.env.SAPPER_TIMESTAMP || Date.now(),
       }),
       new MiniCssExtractPlugin({
-        filename: dev ? '[name].css' : '[name].[contenthash].css',
-        chunkFilename: dev ? '[name].css' : '[name].[contenthash].css'
-      })
+        filename: dev ? "[name].css" : "[name].[contenthash].css",
+        chunkFilename: dev ? "[name].css" : "[name].[contenthash].css",
+      }),
     ].filter(Boolean),
-    devtool: dev && 'inline-source-map'
+    devtool: dev && "inline-source-map",
   },
 
   server: {
     entry: config.server.entry(),
     output: config.server.output(),
-    target: 'node',
+    target: "node",
     resolve: { alias, extensions, mainFields },
-    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    externals: Object.keys(pkg.dependencies).concat("encoding"),
     module: {
       rules: [
         {
           test: /\.(svelte|html)$/,
           use: {
-            loader: 'svelte-loader',
+            loader: "svelte-loader",
             options: {
-              generate: 'ssr',
+              generate: "ssr",
               dev,
-              preprocess
-            }
-          }
+              preprocess,
+            },
+          },
         },
         fileLoader,
-        cssLoader
-      ]
+        cssLoader,
+      ],
     },
-		plugins: [
-			new WebpackModules()
-		],
+    plugins: [new WebpackModules()],
     mode: process.env.NODE_ENV,
     performance: {
-      hints: false // it doesn't matter if server.js is large
-    }
+      hints: false, // it doesn't matter if server.js is large
+    },
   },
 
   serviceworker: {
     entry: config.serviceworker.entry(),
     output: config.serviceworker.output(),
-    mode: process.env.NODE_ENV
-  }
-}
+    mode: process.env.NODE_ENV,
+  },
+};
