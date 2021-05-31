d5c26cc6d7917e7ce396232d465b19974720c861
mazipan <mazipanneh@gmail.com>
2021-05-31 11:53:18 +0000

    🚀 Auto generate by Github Actions

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
   "meta": {
-    "last_updated": "2021-05-29",
+    "last_updated": "2021-05-31",
     "total": 105,
     "credits": "PHPID Community",
     "ui": "https://s.byphp.id/learning"
@@ -51,9 +51,9 @@ module.exports = {
       "slide": "empty",
       "topic": "PHP Native Series: Bikin File Excel",
       "videos": [
-        "empty"
+        "https://www.facebook.com/groups/phpid/permalink/10159219416201101/"
       ],
-      "registrasi": "https://fb.me/e/5H4fQOQA4",
+      "registrasi": "empty",
       "cover": "https://github.com/phpid-jakarta/phpid-learning/raw/master/cover/103.jpg",
       "deskripsi": "empty",
       "tags": [
