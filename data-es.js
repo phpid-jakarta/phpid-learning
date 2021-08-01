4fa68f7dfc051a8c11a862a0df0bc6018ac5869c
mazipan <mazipanneh@gmail.com>
2021-08-01 11:47:44 +0000

    🚀 Auto generate by Github Actions

--- data-es.js
+++ data-es.js
@@ -17,7 +17,7 @@ export default {
         "empty"
       ],
       "registrasi": "https://fb.me/e/1DmUf2gy0",
-      "cover": "https://github.com/phpid-jakarta/phpid-learning/raw/master/cover/not-found.jpg",
+      "cover": "https://github.com/phpid-jakarta/phpid-learning/raw/master/cover/123.jpg",
       "deskripsi": "empty",
       "tags": [
         "php",
