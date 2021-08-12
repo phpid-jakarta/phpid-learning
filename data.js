294cb691bcd0642211619a2ade8de8cb17c38743
mazipan <mazipanneh@gmail.com>
2021-08-12 11:06:25 +0000

    🚀 Auto generate by Github Actions

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
   "meta": {
-    "last_updated": "2021-08-10",
+    "last_updated": "2021-08-12",
     "total": 126,
     "credits": "PHPID Community",
     "ui": "https://s.byphp.id/learning"
