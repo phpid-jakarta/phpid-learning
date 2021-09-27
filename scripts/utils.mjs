f30576964f60cf9a9719e4549669d332d9323e3c
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2021-09-27 09:50:23 +0700

    chore: remove existing constants

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -3,6 +3,7 @@ import fs from 'fs'
 import path from 'path'
 import appRootDir from 'app-root-dir'
 
+import constants from './constants.mjs'
 import packageJsonData from '../package.json'
 
 const ROOT_DIR = appRootDir.get()
@@ -42,7 +43,7 @@ export const getAllSectionsData = async (readmePath) => {
     }
   )
 
-  return readmeContent.match(REGEX_SECTIONS)
+  return readmeContent.match(constants.REGEX_SECTIONS)
 }
 
 export const getCoverUrl = (idx) => {
@@ -71,37 +72,7 @@ export const constructFileContent = (combinedData) => {
   return fileContent
 }
 
-const MONTHNAMES = 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split('_')
-
-const REGEX_SECTIONS = /^#{3}.*\n\n- Waktu.*\n- Pukul.*\n- Pemateri.*\n- Slide.*\n- Video.*\n- Registrasi.*\n- Sesi.*\n- Kategori.*\n- Deskripsi.*\n/gm
-const REGEX_TITLE = /^#{3}.*/gm
-const REGEX_DATE = /^- Waktu.*/gm
-const REGEX_TIME = /^- Pukul.*/gm
-const REGEX_SPEAKER = /^- Pemateri.*/gm
-const REGEX_SLIDE = /^- Slide.*/gm
-const REGEX_VIDEO = /^- Video.*/gm
-const REGEX_REGISTRASI = /^- Registrasi.*/gm
-const REGEX_SESI = /^- Sesi.*/gm
-const REGEX_KATEGORI = /^- Kategori.*/gm
-const REGEX_DESKRIPSI = /^- Deskripsi.*/gm
-
 export const packageJson = packageJsonData
-
-export const constant = {
-  ROOT_DIR,
-  REGEX_SECTIONS,
-  REGEX_TITLE,
-  REGEX_DATE,
-  REGEX_TIME,
-  REGEX_SPEAKER,
-  REGEX_SLIDE,
-  REGEX_VIDEO,
-  REGEX_REGISTRASI,
-  REGEX_SESI,
-  REGEX_KATEGORI,
-  REGEX_DESKRIPSI,
-  MONTHNAMES
-}
 
 const util = {
   writeFile,
@@ -110,8 +81,7 @@ const util = {
   getAllSectionsData,
   getCoverUrl,
   packageJson,
-  constructFileContent,
-  constant
+  constructFileContent
 }
 
 export default util
