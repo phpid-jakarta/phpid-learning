a9c69dace2f056f0d25390b00a7e290f726ec578
Farizal Tri Anugrah <omdjin@gmail.com>
2021-10-07 23:40:52 +0700

    fix: client side navigation on search

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,9 +1,25 @@
 <script>
+  import { goto } from '@sapper/app';
+  import { currentKeyword } from '../store';
+
   export let q;
+
+  const handleSubmit = () => {
+    const keyword = document.getElementById('q').value;
+
+    goto(`/search?q=${keyword}`);
+    currentKeyword.set(keyword);
+  };
 </script>
 
-<form action="/search" class="search-form">
-  <input type="text" name="q" value={q} /><button type="submit">Cari</button>
+<form
+  action="/search"
+  class="search-form"
+  method="GET"
+  on:submit|preventDefault={handleSubmit}
+>
+  <input type="text" name="q" value={q} id="q" />
+  <button type="submit">Cari</button>
 </form>
 
 <style>
