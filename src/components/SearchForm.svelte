fc0562d4d26c4b16415ff5afb379e431d7b725b0
Farizal Tri Anugrah <omdjin@gmail.com>
2021-10-08 19:36:00 +0700

    feat: add search guide for domain specific language

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,12 +1,12 @@
 <script>
-  import { goto } from '@sapper/app';
-  import { currentKeyword } from '../store';
-  import { initMasonry } from '../utils.js';
+  import { goto } from "@sapper/app";
+  import { currentKeyword } from "../store";
+  import { initMasonry } from "../utils.js";
 
   export let q;
 
   const handleSubmit = () => {
-    const keyword = document.getElementById('q').value;
+    const keyword = document.getElementById("q").value;
 
     goto(`/search?q=${keyword}`);
     currentKeyword.set(keyword);
@@ -26,10 +26,22 @@
   <input type="text" name="q" value={q} id="q" />
   <button type="submit">Cari</button>
 </form>
+<p class="small tips">
+  Tips: Kamu juga bisa mencari topik berdasarkan pembicara favoritmu dengan
+  menggunakan kata kunci: <code>from:Nama Pembicara</code>
+</p>
 
 <style>
   .search-form {
     display: flex;
-    padding-bottom: 16px;
+    align-items: center;
+  }
+
+  .search-form button {
+    margin-right: 16px;
+  }
+  .tips {
+    margin-bottom: 16px;
+    font-size: 70%;
   }
 </style>
