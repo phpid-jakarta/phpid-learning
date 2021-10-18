2581fe776f007675a590d1db1fc0ae1b7026a860
Yusuf <hello@iamyuu.dev>
2021-10-18 22:36:05 +0700

    break: remove sapper and init astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  devOptions: {
+    tailwindConfig: './tailwind.config.js',
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
