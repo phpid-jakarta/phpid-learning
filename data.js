a8b4473cacef3d2c359f416f8204e602d15a8aef
peterjkambey <peterjkambey@users.noreply.github.com>
2021-12-03 05:14:09 +0000

    deploy: 23817dcae7b61c93a8743e5fe18229b07e8a71e1

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
   "meta": {
-    "last_updated": "2021-12-02",
+    "last_updated": "2021-12-03",
     "total": 154,
     "credits": "PHPID Community",
     "ui": "https://s.byphp.id/learning"
