3e7ce4825ce187b3692b07cf25d4026806cc0353
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2022-01-01 23:53:04 +0700

    feat: init commit svelte-kit

---
+++ my-app/src/store/index.ts
@@ -0,0 +1,79 @@
+import { writable, readable, derived } from 'svelte/store'
+import { getDistinctTags } from '$lib/utils'
+
+import data from '../api/data-es'
+
+export const originData = readable(data.data, function set() {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  return function destroy() {}
+})
+
+export const allData = writable(data.data)
+
+export const showData = writable(data.data)
+
+export const allDistictTags = derived(allData, $allData => {
+  const r = getDistinctTags($allData)
+  return r
+})
+
+export const currentTag = writable('')
+
+export const allByTags = derived(
+  [currentTag, allData],
+  ([$currentTag, $allData]) => {
+    if ($currentTag) {
+      const r = $allData.filter(i => {
+        return i.tags.includes($currentTag)
+      })
+      return r
+    }
+    return []
+  }
+)
+
+export const currentPage = writable(1)
+
+export const perPage = writable(24)
+
+export const offsetPage = derived(
+  [currentPage, perPage],
+  ([$currentPage, $perPage]) => {
+    return $currentPage * $perPage - $perPage
+  }
+)
+
+export const totalPage = derived(originData, $originData => {
+  return $originData.length
+})
+
+export const activeTheme = writable('')
+
+// Search Store
+export const currentKeyword = writable('')
+
+export const allByKeyword = derived(
+  [currentKeyword, allData],
+  ([$currentKeyword, $allData]) => {
+    if ($currentKeyword) {
+      const speakerFilterPrefix = 'from:'
+      const r = $allData.filter(i => {
+        // search by speaker, by using "from:" prefix on the keyword
+        if ($currentKeyword.startsWith(speakerFilterPrefix)) {
+          const cleanKeyword = $currentKeyword
+            .replace(speakerFilterPrefix, '')
+            .trim()
+            .toLowerCase()
+
+          return i.speaker.toLowerCase().includes(cleanKeyword)
+        }
+
+        // search the topic instead
+        return i.topic.toLowerCase().includes($currentKeyword.toLowerCase())
+      })
+      return r
+    }
+    return []
+  }
+)
+// Eend Search Store
---
+++ my-app/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  // add this section
+  purge: [
+    './src/**/*.html',
+    './src/**/*.svelte'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
