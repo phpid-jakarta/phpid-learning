beb11ab9a420621e83c3901b5b497e652c2a0edd
Faqih Muntashir <itsfaqih@gmail.com>
2022-01-07 06:26:52 +0700

    chore(styling): hide pagination number in smaller screen

--- my-app/src/lib/pagination/Pagination.svelte
+++ my-app/src/lib/pagination/Pagination.svelte
@@ -22,7 +22,7 @@
 </script>
 
 <aside class={className}>
-	<ul class="flex flex-wrap justify-center items-center gap-3 tracking-content md:text-sm">
+	<ul class="flex flex-wrap md:justify-center justify-between items-center gap-3 tracking-content md:text-sm">
 		<li class="relative">
 			<button
 				class="text-gray-600 dark:text-gray-300 flex items-center gap-x-1.5 rounded-lg bg-gradient-to-r from-gray-600/10 to-gray-500/10 dark:from-gray-600/25 dark:to-gray-500/25 py-1.5 pl-3 pr-4 {$currentPage !==
@@ -58,7 +58,7 @@
 		</li>
 
 		{#each createArray(pageCount) as i}
-			<li class="relative">
+			<li class="relative md:block hidden">
 				<button
 					class="bg-gradient-to-r rounded-lg py-1.5 px-3 {$currentPage === i + 1
 						? 'text-white from-blue-600/75 to-blue-500/75 dark:from-blue-600/25 dark:to-blue-500/25'
