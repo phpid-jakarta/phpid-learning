f6a81af5672ae6a74a37ec46afc044b94bfb8405
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2022-01-06 22:14:42 +0700

    chore(style): add some stylings

--- my-app/src/lib/event-item/Tags.svelte
+++ my-app/src/lib/event-item/Tags.svelte
@@ -1,50 +1,33 @@
 <script>
-  import { goto } from '$app/navigation';
-  import { currentTag } from "../../store";
-  import { getTagWithColor, initMasonry } from "../utils";
+	import { goto } from '$app/navigation';
+	import { currentTag } from '../../store';
+	import { getTagWithColor, initMasonry } from '../utils';
 
-  export let tags;
-  const tags_w_color = getTagWithColor(tags);
+	export let tags;
+	const tags_w_color = getTagWithColor(tags);
 
-  const handleClickTag = (e, tag) => {
-    e.preventDefault();
-    goto(`/tag/${tag}`);
-    currentTag.set(tag);
+	const handleClickTag = (e, tag) => {
+		e.preventDefault();
+		goto(`/tag/${tag}`);
+		currentTag.set(tag);
 
-    setTimeout(() => {
-      initMasonry()
-    }, 500);
-  };
+		setTimeout(() => {
+			initMasonry();
+		}, 500);
+	};
 </script>
 
-<style>
-  .badge {
-    margin-left: 0 !important;
-    margin-right: 0.5em;
-  }
-</style>
-
-<div class="badge-group">
-  {#if tags_w_color && tags_w_color.length > 0}
-    {#each tags_w_color as item (item)}
-      {#if item.isMapped}
-        <a
-          class="badge"
-          href={`/tag/${item.tag}`}
-          alt={item.tag}
-          on:click={e => handleClickTag(e, item.tag)}
-          style="background-color: {item.bg};color: {item.fg};">
-          {item.tag}
-        </a>
-      {:else}
-        <a
-          class="badge {item.classes}"
-          on:click={e => handleClickTag(e, item.tag)}
-          href={`/tag/${item.tag}`}
-          alt={item.tag}>
-          {item.tag}
-        </a>
-      {/if}
-    {/each}
-  {/if}
+<div class="flex flex-wrap gap-2">
+	{#if tags_w_color && tags_w_color.length > 0}
+		{#each tags_w_color as item (item)}
+			<a
+				class="cursor-pointer text-gray-500 dark:text-gray-400 hover:underline"
+				on:click={(e) => handleClickTag(e, item.tag)}
+				href={`/tag/${item.tag}`}
+				alt={item.tag}
+			>
+				#{item.tag}
+			</a>
+		{/each}
+	{/if}
 </div>
