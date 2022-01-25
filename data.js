9a641f9c46847f91110b2262a4583d464e16eab2
mazipan <mazipan@users.noreply.github.com>
2022-01-25 05:05:13 +0000

    deploy: 253d7d4accce8b977896b79e3027097d514eb1de

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
   "meta": {
-    "last_updated": "2022-01-24",
+    "last_updated": "2022-01-25",
     "total": 165,
     "credits": "PHPID Community",
     "ui": "https://s.byphp.id/learning"
